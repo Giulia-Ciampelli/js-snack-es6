48c119693a83d8438d01a4d1ba88d20c2236239b
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2024-10-11 17:34:01 +0200

    docs: creation of array, function skeleton

--- snack-bonus/assets/main.js
+++ snack-bonus/assets/main.js
@@ -4,8 +4,41 @@ console.log('im alive three');
 // Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
 // La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
 
-let result = [];
+// variabili
 
-function numBetween(arr, a, b) {
+let classes = [
+    {
+        name: 'Class 1',
+        grade: 2
+    },
+    {
+        name: 'Class 2',
+        grade: 5
+    },
+    {
+        name: 'Class 3',
+        grade: 1
+    },
+    {
+        name: 'Class 4',
+        grade: 6
+    },
+    {
+        name: 'Class 5',
+        grade: 3
+    }
+];
 
+function indexBetween(arr, a, b) {
+    
+    // accesso agli indici
+    arr[a];
+
+    // condizionale per tenere a minore di b
+    if (a < b) {
+        alert('Errore: "a" devessere minore di "b"');
+        return;
+    }
 }
+
+indexBetween(classes, 2);
