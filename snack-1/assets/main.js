bb791682d238b3eb0da4042ff4c7c92554e437ff
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2024-10-11 15:07:29 +0200

    docs: fill html, object array blueprint

--- snack-1/assets/main.js
+++ snack-1/assets/main.js
@@ -4,3 +4,30 @@ console.log('im alive');
 // Creare un array di oggetti:
 // Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 // Stampare a schermo la bici con peso minore.
+
+const raceBikes = [
+    {
+        name: '',
+        weight: 
+    },
+    {
+        name: '',
+        weight: 
+    },
+    {
+        name: '',
+        weight: 
+    },
+    {
+        name: '',
+        weight: 
+    },
+    {
+        name: '',
+        weight: 
+    },
+    {
+        name: '',
+        weight: 
+    }
+];
