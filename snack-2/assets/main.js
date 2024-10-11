05408a9e17ceb3adefe9092ba4336826525a326b
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2024-10-11 16:38:58 +0200

    docs: add object array

--- snack-2/assets/main.js
+++ snack-2/assets/main.js
@@ -6,3 +6,31 @@ console.log('im alive too');
 // Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
 // Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
 // Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+const teams = [
+    {
+        name: 'Barcelona',
+        fouls: 0,
+        goals: 0
+    },
+    {
+        name: 'PSG',
+        fouls: 0,
+        goals: 0
+    },
+    {
+        name: 'Juventus',
+        fouls: 0,
+        goals: 0
+    },
+    {
+        name: 'Bayern',
+        fouls: 0,
+        goals: 0
+    },
+    {
+        name: 'Manchester',
+        fouls: 0,
+        goals: 0
+    }
+];
