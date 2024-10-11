14df51d530c3d452cdb98d8f3c94500d605b2ba5
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2024-10-11 17:06:39 +0200

    docs: function and array names

--- snack-bonus/assets/main.js
+++ snack-bonus/assets/main.js
@@ -3,3 +3,9 @@ console.log('im alive three');
 // Snack 3 (Bonus)
 // Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
 // La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
+
+let result = [];
+
+function numBetween(arr, a, b) {
+
+}
