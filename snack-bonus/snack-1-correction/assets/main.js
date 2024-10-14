015b077560e8487ae29508524cc5517f10dba6a9
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2024-10-14 09:50:30 +0200

    docs: snack 1 correction

---
+++ snack-bonus/snack-1-correction/assets/main.js
@@ -0,0 +1,45 @@
+console.log('im alive');
+
+// Snack 1
+// Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo la bici con peso minore.
+
+// variabili di output
+const outputElement = document.getElementById('output');
+
+const raceBikes = [
+    {
+        name: 'Kabon',
+        weight: 8.3
+    },
+    {
+        name: 'Savadeck',
+        weight: 8.4
+    },
+    {
+        name: 'Hiland',
+        weight: 13.8
+    },
+    {
+        name: 'Kootu',
+        weight: 9.4
+    }
+];
+
+// variabile da inserire dopo
+let lightestBike = raceBikes[0];
+
+// ciclo for per cercare in tutto l'array
+for (i = 0; i < raceBikes.length; i++) {
+    const bike = raceBikes[i];
+    let weight = bike.weight;
+
+    // comparazione peso bici
+    if (weight < lightestBike.weight) {
+        lightestBike = bike;
+    }
+}
+
+// stampa in schermo
+outputElement.innerHTML = `Bici più leggera: ${lightestBike.name}, ${lightestBike.weight}kg`;
---
+++ snack-bonus/snack-2-correction/assets/main.js
@@ -0,0 +1,53 @@
+console.log('im alive too');
+
+// Snack2
+// Creare un array di oggetti di squadre di calcio.
+// Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+// Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+// Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+// Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+// variabili
+let result = [];
+
+const teams = [
+    {
+        name: 'Barcelona',
+        fouls: 0,
+        goals: 0
+    },
+    {
+        name: 'PSG',
+        fouls: 0,
+        goals: 0
+    },
+    {
+        name: 'Juventus',
+        fouls: 0,
+        goals: 0
+    },
+    {
+        name: 'Bayern',
+        fouls: 0,
+        goals: 0
+    },
+    {
+        name: 'Manchester',
+        fouls: 0,
+        goals: 0
+    }
+];
+
+// funzione random da richiamare nel loop
+function randomNum() {
+    return Math.floor(Math.random() * 100) + 1;
+}
+
+// ciclo for per passare la funzione random sulle proprietà fouls e goals
+for (i = 0; i < teams.length; i++) {
+    const team = teams[i];
+    team.fouls = randomNum();
+    team.goals = randomNum();
+    result = `Nome: ${team.name}; Falli subiti: ${team.fouls}`;
+    console.log(result);
+}
